f6351aacb438103afd2ad4826f08596e78901354
kyosuke ito <kyosuke.ito@gmail.com>
2021-12-11 14:29:06 +0900

    add basic styling for chat room

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,20 +2,28 @@ import React, { useState, useEffect } from "react";
 import { Avatar } from "@material-ui/core";
 import "./SidebarChat.css";
 
-const SidebarChat = () => {
+const SidebarChat = ({ addNewChat }) => {
   const [seed, setSeed] = useState("");
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
 
-  return (
+  const createChat = () => {
+    const roomName = prompt("Please enter name for chat");
+  };
+
+  return !addNewChat ? (
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
         <h2>Room name</h2>
         <p>Last message...</p>
       </div>
+    </div>
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add new Chat</h2>
     </div>
   );
 };
